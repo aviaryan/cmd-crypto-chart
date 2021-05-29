19038455c4e8296f0c3b13b45b14e777c756a398
Avi Aryan <avi.aryan123@gmail.com>
2021-05-29 22:56:55 +0530

    call github API successfully
    for test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use serde::Deserialize;
+use reqwest::Error;
+
+#[derive(Deserialize, Debug)]
+struct User {
+    login: String,
+    id: u32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+
+    // set user agent
+    let client = reqwest::Client::builder()
+        .user_agent("aviaryan")
+        .build()?;
+
+    // from https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html
+    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
+                              owner = "rust-lang-nursery",
+                              repo = "rust-cookbook");
+    println!("{}", request_url);
+    let response = client.get(&request_url).send().await?;
+
+    // println!("Status: {}", response.status());
+    // println!("Status: {:#?}", response.text().await?);
+
+    let users: Vec<User> = response.json().await?;
+    println!("{:?}", users);
+    Ok(())
 }
