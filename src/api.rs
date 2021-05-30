0d46826b1adb9438bc372970ca2678958ac7b9a1
Avi Aryan <avi.aryan123@gmail.com>
2021-05-30 11:15:55 +0530

    move API call to another function and have it working

---
+++ src/api.rs
@@ -0,0 +1,37 @@
+mod utils;
+
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct PriceData {
+	pub prices: Vec<(u64, f32)>,
+}
+
+pub async fn get_prices() -> Result<PriceData, Error> {
+    let unix_time = utils::get_unix_time();
+    // println!("{}", unix_time);
+    // Ok(())
+
+    // set user agent
+    let client = reqwest::Client::builder().build()?;
+
+    // from https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html
+    let request_url = format!(
+        "https://api.coingecko.com/api/v3/coins/{coin}/market_chart/range?vs_currency=usd&from={from}&to={to}",
+        coin = "bitcoin",
+        from = unix_time / 1000 - (24 * 60 * 60),
+        // ^ from current time - 8hr to {NOW}
+        to = unix_time / 1000
+    );
+    println!("{}", request_url);
+    let response = client.get(&request_url).send().await?;
+
+    // println!("Status: {}", response.status());
+    // println!("Status: {:#?}", response.text().await?);
+
+    let prices: PriceData = response.json().await?;
+    println!("{:?}", prices);
+    println!("{}", prices.prices.len());
+	Ok(prices)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,44 @@
 //     println!("Hello, world!");
 // }
 
-mod utils;
+// mod utils;
+mod api;
 
 use reqwest::Error;
-use serde::Deserialize;
+// use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-struct PriceData {
-    prices: Vec<(u64, f32)>,
-}
+// #[derive(Deserialize, Debug)]
+// struct PriceData {
+//     prices: Vec<(u64, f32)>,
+// }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let unix_time = utils::get_unix_time();
-    // println!("{}", unix_time);
-    // Ok(())
+    let prices = api::get_prices().await?;
+    // let unix_time = utils::get_unix_time();
+    // // println!("{}", unix_time);
+    // // Ok(())
 
-    // set user agent
-    let client = reqwest::Client::builder().build()?;
+    // // set user agent
+    // let client = reqwest::Client::builder().build()?;
 
-    // from https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html
-    let request_url = format!(
-        "https://api.coingecko.com/api/v3/coins/{coin}/market_chart/range?vs_currency=usd&from={from}&to={to}",
-        coin = "bitcoin",
-        from = unix_time / 1000 - (24 * 60 * 60),
-        // ^ from current time - 8hr to {NOW}
-        to = unix_time / 1000
-    );
-    println!("{}", request_url);
-    let response = client.get(&request_url).send().await?;
+    // // from https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html
+    // let request_url = format!(
+    //     "https://api.coingecko.com/api/v3/coins/{coin}/market_chart/range?vs_currency=usd&from={from}&to={to}",
+    //     coin = "bitcoin",
+    //     from = unix_time / 1000 - (24 * 60 * 60),
+    //     // ^ from current time - 8hr to {NOW}
+    //     to = unix_time / 1000
+    // );
+    // println!("{}", request_url);
+    // let response = client.get(&request_url).send().await?;
 
-    // println!("Status: {}", response.status());
-    // println!("Status: {:#?}", response.text().await?);
+    // // println!("Status: {}", response.status());
+    // // println!("Status: {:#?}", response.text().await?);
 
-    let prices: PriceData = response.json().await?;
+    // let prices: PriceData = response.json().await?;
     println!("{:?}", prices);
     println!("{}", prices.prices.len());
+
     Ok(())
 }
